17d7710f613bbba683ae6f63cd7e3e83d18ea6a4
Usman Shabbir <usman.shabbir@gmail.com>
2018-05-31 10:11:59 +0100

    CDD-1559 Removed bootstrap play and using latest version of customs-api-common library (#88)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,17 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapPlay25Version = "1.5.0"
   val hmrcTestVersion = "3.0.0"
   val scalaTestVersion = "3.0.4"
   val scalatestplusVersion = "2.0.1"
   val mockitoVersion = "2.6.2"
   val pegdownVersion = "1.6.0"
   val wireMockVersion = "2.10.1"
-  val customsApiCommonVersion = "1.25.0"
+  val customsApiCommonVersion = "1.26.0"
   val circuitBreakerVersion = "3.2.0"
   val testScope = "test,it"
 
   val xmlResolver = "xml-resolver" % "xml-resolver" % "1.2"
-
-  val bootstrapPlay25 = "uk.gov.hmrc" %% "bootstrap-play-25" % bootstrapPlay25Version
 
   val hmrcTest = "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % testScope
 

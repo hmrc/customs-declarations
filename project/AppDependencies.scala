9817750e9b44f292617e01a60df10acbbeeb4944
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2019-02-25 08:57:19 +0000

    CDD-1889: changed from playReactiveMongo to simpleReactiveMongo & updated some other dependencies (#215)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,20 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcTestVersion = "3.3.0"
+  private val hmrcTestVersion = "3.5.0-play-25"
   private val scalaTestVersion = "3.0.5"
   private val scalatestplusVersion = "2.0.1"
-  private val mockitoVersion = "2.22.0"
-  private val pegdownVersion = "1.6.0"
-  private val wireMockVersion = "2.18.0"
+  private val mockitoVersion = "2.24.5"
+  private val wireMockVersion = "2.21.0"
   private val customsApiCommonVersion = "1.36.0"
   private val circuitBreakerVersion = "3.3.0"
-  private val playReactivemongoVersion = "6.2.0"
-  private val reactivemongoTestVersion = "3.1.0"
+  private val simpleReactiveMongoVersion = "7.12.0-play-25"
+  private val reactiveMongoTestVersion = "4.7.0-play-25"
   private val testScope = "test,it"
 
   val hmrcTest = "uk.gov.hmrc" %% "hmrctest" % hmrcTestVersion % testScope
 
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % testScope
-
-  val pegDown = "org.pegdown" % "pegdown" % pegdownVersion % testScope
 
   val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusVersion % testScope
 
@@ -32,8 +29,8 @@ object AppDependencies {
 
   val circuitBreaker = "uk.gov.hmrc" %% "reactive-circuit-breaker" % circuitBreakerVersion
 
-  val playReactiveMongo = "uk.gov.hmrc" %% "play-reactivemongo" % playReactivemongoVersion
+  val simpleReactiveMongo = "uk.gov.hmrc" %% "simple-reactivemongo" % simpleReactiveMongoVersion
 
-  val reactiveMongoTest = "uk.gov.hmrc" %% "reactivemongo-test" % reactivemongoTestVersion % testScope
+  val reactiveMongoTest = "uk.gov.hmrc" %% "reactivemongo-test" % reactiveMongoTestVersion % testScope
 
 }

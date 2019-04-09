a4fe0d05c00d29ef53b5b8b8c8ae3a756b76b84d
Rashad Mughal <rashadmughal@users.noreply.github.com>
2019-04-09 14:04:10 +0100

    CDD-2178: Remove AnyContentAsXml from payload.

--- app/uk/gov/hmrc/customs/declaration/services/NrsService.scala
+++ app/uk/gov/hmrc/customs/declaration/services/NrsService.scala
@@ -24,6 +24,7 @@ import java.security.MessageDigest.getInstance
 import javax.inject.{Inject, Singleton}
 import com.google.common.io.BaseEncoding.base64
 import play.api.libs.json.{JsObject, JsString, JsValue}
+import play.api.mvc.AnyContentAsXml
 import uk.gov.hmrc.customs.declaration.connectors.NrsConnector
 import uk.gov.hmrc.customs.declaration.controllers.CustomHeaderNames.Authorization
 import uk.gov.hmrc.customs.declaration.logging.DeclarationsLogger
@@ -47,19 +48,21 @@ class NrsService @Inject()(logger: DeclarationsLogger,
 
   def send[A](implicit vpr: ValidatedPayloadRequest[A], hc: HeaderCarrier): Future[NrSubmissionId] = {
 
+    val payloadAsString = vpr.request.body.asInstanceOf[AnyContentAsXml].xml.toString
+
     val nrsMetadata = new NrsMetadata(businessId = businessIdValue,
-      notableEvent = notableEventValue,
-      payloadContentType = applicationXml,
-      payloadSha256Checksum = sha256Hash(vpr.request.body.toString), // This should come from the end user NOT us
-      userSubmissionTimestamp = dateTimeService.nowUtc().toString,
-      userAuthToken = vpr.headers.get(Authorization).getOrElse(""),
-      identityData = vpr.authorisedAs.retrievalData.get, // this should always be populated when nrs is enabled and called
-      headerData = new JsObject(vpr.request.headers.toMap.map(x => x._1 -> JsString(x._2 mkString ","))),
-      searchKeys = JsObject(Map[String, JsValue](conversationIdKey -> JsString(vpr.conversationId.toString))),
-      nrsSubmissionId = vpr.conversationId.toString
+    notableEvent = notableEventValue,
+    payloadContentType = applicationXml,
+    payloadSha256Checksum = sha256Hash(payloadAsString), // This should come from the end user NOT us
+    userSubmissionTimestamp = dateTimeService.nowUtc().toString,
+    userAuthToken = vpr.headers.get(Authorization).getOrElse(""),
+    identityData = vpr.authorisedAs.retrievalData.get, // this should always be populated when nrs is enabled and called
+    headerData = new JsObject(vpr.request.headers.toMap.map(x => x._1 -> JsString(x._2 mkString ","))),
+    searchKeys = JsObject(Map[String, JsValue](conversationIdKey -> JsString(vpr.conversationId.toString))),
+    nrsSubmissionId = vpr.conversationId.toString
     )
 
-    val nrsPayload: NrsPayload = NrsPayload(base64().encode(vpr.request.body.toString.getBytes(UTF_8)), nrsMetadata)
+    val nrsPayload: NrsPayload = NrsPayload(base64().encode(payloadAsString.getBytes(UTF_8)), nrsMetadata)
 
     nrsConnector.send(nrsPayload, vpr.requestedApiVersion).recoverWith {
         case e: HttpException =>

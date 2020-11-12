c208ea07f0f9f3c73125ffaeb45f62ac968bf17b
Paul Blake <32386617+paulblakehmrc@users.noreply.github.com>
2020-11-12 11:20:18 +0000

    CDD-2989: bootstrap-play update to bump http-verbs and remove outdated terminology (#371)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
   private val scalatestplusVersion = "4.0.3"
   private val mockitoVersion = "3.5.9"
   private val wireMockVersion = "2.27.2"
-  private val customsApiCommonVersion = "1.53.0"
+  private val customsApiCommonVersion = "1.54.0"
   private val playJsonJodaVersion = "2.8.1"
   private val simpleReactiveMongoVersion = "7.30.0-play-27"
   private val reactiveMongoTestVersion = "4.21.0-play-27"

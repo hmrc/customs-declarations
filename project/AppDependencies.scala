e3326870d8d8319d492401851980f8bea7aa87f2
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-02-08 08:56:30 +0000

    Update dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,33 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val scalatestplusVersion = "5.1.0"
-  private val customsApiCommonVersion = "1.57.0"
-  private val playJsonJodaVersion = "2.9.2"
-  private val simpleReactiveMongoVersion = "8.0.0-play-28"
-  private val reactiveMongoTestVersion = "5.0.0-play-28"
   private val testScope = "test,it"
 
-  val scalaTestPlusPlay = "org.scalatestplus.play" %% "scalatestplus-play" % scalatestplusVersion % testScope
-
-  val flexmark = "com.vladsch.flexmark" % "flexmark-all" % "0.35.10"  % testScope
-
-  val mockito = "org.scalatestplus" %% "mockito-3-4" % "3.2.9.0" % testScope
-
-  val wireMock = "com.github.tomakehurst" % "wiremock-standalone" % "2.27.1" % testScope
-
-  val customsApiCommon = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion withSources()
-
-  val customsApiCommonTests = "uk.gov.hmrc" %% "customs-api-common" % customsApiCommonVersion % testScope classifier "tests"
-
-  val playJsonJoda = "com.typesafe.play" %% "play-json-joda" % playJsonJodaVersion
-
-  val hmrcMongo = "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"      % "0.73.0"
-
-  val hmrcMongoTest = "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.73.0"
-
-  val silencerPlugin = compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full)
-
-  val silencerLib = "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
+  val scalaTestPlusPlay     = "org.scalatestplus.play"                     %% "scalatestplus-play"      % "5.1.0"                 % testScope
+  val flexmark              = "com.vladsch.flexmark"                       %  "flexmark-all"            % "0.35.10"               % testScope
+  val mockito               = "org.scalatestplus"                          %% "mockito-3-4"             % "3.2.10.0"              % testScope
+  val wireMock              = "com.github.tomakehurst"                     %  "wiremock-standalone"     % "2.27.2"                % testScope
+  val customsApiCommon      = "uk.gov.hmrc"                                %% "customs-api-common"      % "1.57.0"                withSources()
+  val customsApiCommonTests = "uk.gov.hmrc"                                %% "customs-api-common"      % "1.57.0"                % testScope classifier "tests"
+  val playJsonJoda          = "com.typesafe.play"                          %% "play-json-joda"          % "2.9.4"
+  val hmrcMongo             = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-play-28"      % "0.74.0"
+  val hmrcMongoTest         = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-test-play-28" % "0.74.0"
+  val silencerPlugin        = compilerPlugin("com.github.ghik" %  "silencer-plugin"         % "1.7.11"                cross CrossVersion.full)
+  val silencerLib           = "com.github.ghik"                            %  "silencer-lib"            % "1.7.11"                % Provided cross CrossVersion.full
 
 }

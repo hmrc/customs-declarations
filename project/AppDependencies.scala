4ad712aba19e509c629ab599f64a7b5b7e8cd854
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-02-08 09:35:50 +0000

    Fix Jenkins pr-builder failure

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,6 +11,7 @@ object AppDependencies {
   val customsApiCommon      = "uk.gov.hmrc"                                %% "customs-api-common"      % "1.57.0"   withSources()
   val customsApiCommonTests = "uk.gov.hmrc"                                %% "customs-api-common"      % "1.57.0"   % testScope classifier "tests"
   val playJsonJoda          = "com.typesafe.play"                          %% "play-json-joda"          % "2.9.4"
+  val hmrcMongo             = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-play-28"      % "0.74.0"
   val hmrcMongoTest         = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-test-play-28" % "0.74.0"   % testScope
   val silencerPlugin        = compilerPlugin("com.github.ghik" %  "silencer-plugin"         % "1.7.11"   cross CrossVersion.full)
   val silencerLib           = "com.github.ghik"                            %  "silencer-lib"            % "1.7.11"   % Provided cross CrossVersion.full

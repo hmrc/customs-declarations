242cea43a29fa51efda2a72f54f7697f26110418
Tawanda Siyamachira <62283841+TSiyamachiraQA@users.noreply.github.com>
2023-05-16 13:42:25 +0100

    DCWL-802 Address platops-pr-bot suggestions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,5 +12,6 @@ object AppDependencies {
   val customsApiCommonTests = "uk.gov.hmrc"                                %% "customs-api-common"      % "1.59.0"   % testScope classifier "tests"
   val hmrcMongo             = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-play-28"      % "1.2.0"
   val hmrcMongoTest         = "uk.gov.hmrc.mongo"                          %% "hmrc-mongo-test-play-28" % "1.2.0"   % testScope
+  val hmrcBootstrapTest         = "uk.gov.hmrc"                                %% "bootstrap-test-play-28"  % "7.15.0" % testScope
 
 }

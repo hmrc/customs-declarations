47101f5b3817bb424c210789b5a64ecc0836a372
sunil-sethumadhavan <171044301+sunil-sethumadhavan@users.noreply.github.com>
2025-07-28 14:48:11 +0100

    DCWL-3669: Dependency upgrade to fix the jackson-core vulnerability

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,9 +6,9 @@ object AppDependencies {
 
   val playSuffix = "-play-30"
 
-  val bootstrapVersion = "9.13.0"
-  val hmrcMongoVersion = "2.6.0"
-  val scalamockVersion = "7.2.0"
+  val bootstrapVersion = "9.18.0"
+  val hmrcMongoVersion = "2.7.0"
+  val scalamockVersion = "7.4.0"
 
   val bootstrapBackendPlay30        = "uk.gov.hmrc"        %% s"bootstrap-backend$playSuffix" % bootstrapVersion
   val cats                          = "org.typelevel"      %% "cats-core"                     % "2.13.0"

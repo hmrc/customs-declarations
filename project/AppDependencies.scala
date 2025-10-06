0d5d61c03f757d894bb0d8bb00b5a0f82db93ca4
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2025-10-06 10:28:18 +0100

    DCWL-3962 | Update bootstrap

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val playSuffix = "-play-30"
 
-  val bootstrapVersion = "9.18.0"
+  val bootstrapVersion = "9.19.0"
   val hmrcMongoVersion = "2.7.0"
   val scalamockVersion = "7.4.0"
 
